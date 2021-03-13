8c7e0b5aedd356c12b6f67c19141c0731a98bd7a
Biliane Moreira <bilianemoreira@gmail.com>
2021-03-13 15:51:48 +0100

    fixed title alignment

--- src/screens/About/index.tsx
+++ src/screens/About/index.tsx
@@ -5,7 +5,7 @@ import Hero from './components/Hero'
 export default function About() {
   return (
     <>
-      <div className="title">About Me</div>
+      <div className="title is-flex is-justify-content-center mt-2">About Me</div>
       <Hero />
       <Drawings />
     </>
