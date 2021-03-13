9b91f61b44eef58b0467e20bc5bb41f945b75803
Biliane Moreira <bilianemoreira@gmail.com>
2021-03-13 16:21:06 +0100

    Fixed intro margins

--- src/components/UI/Intro.tsx
+++ src/components/UI/Intro.tsx
@@ -4,7 +4,7 @@ export default function Intro() {
   return (
     <div className="columns is-centered">
       <div className="column">
-        <h2 className="subtitle" style={{ textAlign: 'center' }}>
+        <h2 className="subtitle" style={{ textAlign: 'center', margin: '80px 0 62px 0', padding: '0 250px' }}>
           I’m Biliane Moreira, UI/UX Designer based in Stockholm. I love
           creating design focused on the user experience, and intuitive
           interfaces, making products that users love.
