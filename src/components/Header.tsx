3c54e197591df0e1fc302afefdd0e087091ebe16
Biliane Moreira <bilianemoreira@gmail.com>
2021-03-13 16:48:26 +0100

    fixed hamburger menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,8 @@ import logo from '../assets/logo.png'
 import Links from './UI/Links'
 
 export default function Header() {
+  const [isActive, setIsActive] = React.useState(false)
+
   return (
     <nav className="navbar mt-4" role="navigation" aria-label="main navigation">
       <div className="navbar-brand">
@@ -13,10 +15,11 @@ export default function Header() {
 
         <span
           role="button"
-          className="navbar-burger"
+          className={`navbar-burger ${isActive ? 'is-active' : ''}`}
           aria-label="menu"
           aria-expanded="false"
           data-target="navbarBasicExample"
+          onClick={() => setIsActive(!isActive)}
         >
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
@@ -24,7 +27,7 @@ export default function Header() {
         </span>
       </div>
 
-      <div id="navbarBasicExample" className="navbar-menu">
+      <div id="navbarBasicExample" className={`navbar-menu ${isActive ? 'is-active' : ''}`}>
         <div className="navbar-end">
           <Links />
         </div>
